8b6e207ce6d432a399629dc986860451b541ee4d
BenNoxXD <90966148+BenNoxXD@users.noreply.github.com>
2024-11-16 18:24:57 +0100

    Update install_ps5_server.sh

--- install_ps5_server.sh
+++ install_ps5_server.sh
@@ -6,6 +6,7 @@ sudo systemctl stop ps5host
 sudo systemctl disable ps5host
 sudo rm -r /PS5-UMTX-Jailbreak/
 # download new version
+cd /home/
 git clone https://github.com/idlesauce/PS5-UMTX-Jailbreak
 cd PS5-UMTX-Jailbreak
 wget https://raw.githubusercontent.com/idlesauce/PS5-Exploit-Host/refs/heads/main/simple_server.py
@@ -15,7 +16,7 @@ cat > /etc/systemd/system/ps5host.service <<- "EOF"
 Description=PS5 UMTX Host idlesauce
 
 [Service]
-WorkingDirectory=/home/pi/PS5-UMTX-Jailbreak/
+WorkingDirectory=/home/PS5-UMTX-Jailbreak/
 ExecStart=/usr/bin/python3 simple_server.py
 
 [Install]

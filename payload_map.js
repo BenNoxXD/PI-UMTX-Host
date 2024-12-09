0c75fa47b94d9b95159b16abf84931786896cc7f
BenNoxXD <flippydiode@gmail.com>
2024-12-09 18:04:07 +0100

    Update

---
+++ payload_map.js
@@ -0,0 +1,124 @@
+// @ts-check
+
+const CUSTOM_ACTION_APPCACHE_REMOVE = "appcache-remove";
+
+/**
+ * @typedef {Object} PayloadInfo
+ * @property {string} displayTitle
+ * @property {string} description
+ * @property {string} fileName - path relative to the payloads folder
+ * @property {string} author
+ * @property {string} projectSource
+ * @property {string} binarySource - should be direct download link to the included version, so that you can verify the hashes
+ * @property {string} version
+ * @property {string[]?} [supportedFirmwares] - optional, these are interpreted as prefixes, so "" would match all, and "4." would match 4.xx, if not set, the payload is assumed to be compatible with all firmwares
+ * @property {number?} [toPort] - optional, if the payload should be sent to "127.0.0.1:<port>" instead of loading directly, if specified it'll show up in webkit-only mode too
+ * @property {string?} [customAction]
+ */
+
+/**
+ * @type {PayloadInfo[]}
+*/
+const payload_map = [
+    // { // auto-loaded
+    //     displayTitle: "PS5 Payload ELF Loader",
+    //     description: "Uses port 9021. Persistent network elf loader",
+    //     fileName: "elfldr.elf",
+    //     author: "john-tornblom",
+    //     projectSource: "https://github.com/ps5-payload-dev/elfldr",
+    //     binarySource: "https://github.com/ps5-payload-dev/pacbrew-repo/actions/runs/11597570082",
+    //     version: "?",
+    //     supportedFirmwares: ["1.", "2.", "3.", "4.", "5."]
+    // },
+    /*{
+        displayTitle: "ps5-kstuff",
+        description: "FPKG enabler",
+        fileName: "ps5-kstuff.bin",
+        author: "sleirsgoevy",
+        projectSource: "https://github.com/sleirsgoevy/ps4jb-payloads/tree/bd-jb/ps5-kstuff",
+        binarySource: "https://github.com/sleirsgoevy/ps4jb2/blob/3e6053c3e4c691a9ccdc409172293a81de00ad7f/ps5-kstuff.bin",
+        version: "3e6053c",
+        supportedFirmwares: ["3.", "4."]
+    },*/
+    {
+        displayTitle: "websrv",
+        description: "Uses john-tornblom's elfldr. Custom homebrew loader. Runs on port 8080.",
+        fileName: "websrv.elf",
+        author: "john-tornblom",
+        projectSource: "https://github.com/ps5-payload-dev/websrv/releases",
+        binarySource: "https://github.com/ps5-payload-dev/websrv/releases",
+        version: "0.14",
+        toPort: 9021
+    },
+    {
+        displayTitle: "ftpsrv",
+        description: "Uses john-tornblom's elfldr. FTP server. Runs on port 2121.",
+        fileName: "ftpsrv.elf",
+        author: "john-tornblom",
+        projectSource: "https://github.com/ps5-payload-dev/ftpsrv",
+        binarySource: "https://github.com/ps5-payload-dev/pacbrew-repo/actions/runs/11597570082",
+        version: "0.11",
+        toPort: 9021
+    },
+    {
+        displayTitle: "klogsrv",
+        description: "Uses john-tornblom's elfldr. Klog server. Runs on port 3232.",
+        fileName: "klogsrv.elf",
+        author: "john-tornblom",
+        projectSource: "https://github.com/ps5-payload-dev/klogsrv/releases",
+        binarySource: "https://github.com/ps5-payload-dev/pacbrew-repo/actions/runs/11597570082",
+        version: "0.5",
+        toPort: 9021
+    },
+    {
+        displayTitle: "shsrv",
+        description: "Uses john-tornblom's elfldr. Telnet shell server. Runs on port 2323.",
+        fileName: "shsrv.elf",
+        author: "john-tornblom",
+        projectSource: "https://github.com/ps5-payload-dev/shsrv/releases",
+        binarySource: "https://github.com/ps5-payload-dev/pacbrew-repo/actions/runs/11597570082",
+        version: "0.12",
+        toPort: 9021
+    },
+    {
+        displayTitle: "gdbsrv",
+        description: "Uses john-tornblom's elfldr. GDB server. Runs on port 2159.",
+        fileName: "gdbsrv.elf",
+        author: "john-tornblom",
+        projectSource: "https://github.com/ps5-payload-dev/gdbsrv/releases",
+        binarySource: "https://github.com/ps5-payload-dev/pacbrew-repo/actions/runs/11597570082",
+        version: "0.4-1",
+        toPort: 9021
+    },
+    {
+        displayTitle: "ps5debug",
+        description: "Debugger, open source version by DizzRL",
+        fileName: "ps5debug_dizz.elf",
+        author: "Dizz, astrelsky, John Tornblom, SiSTR0, golden, idlesauce",
+        projectSource: "https://github.com/idlesauce/ps5debug",
+        binarySource: "https://github.com/idlesauce/ps5debug/releases/download/v0.0.1/ps5debug.elf",
+        version: "0.0.1",
+        toPort: 9021
+    },
+    {
+        displayTitle: "ps5-versions",
+        description: "Shows kernel build, os and sdk versions",
+        fileName: "ps5-versions.elf",
+        author: "SiSTRo",
+        projectSource: "https://github.com/SiSTR0/ps5-versions",
+        binarySource: "https://github.com/SiSTR0/ps5-versions/releases/download/v1.0/ps5-versions.elf",
+        version: "1.0",
+        supportedFirmwares: ["1.", "2.", "3.", "4."]
+    },
+    {
+        displayTitle: "Browser appcache remover",
+        description: "Deletes for only the current user in webkit-only mode",
+        fileName: "",
+        author: "Storm21CH, idlesauce",
+        projectSource: "https://github.com/Storm21CH/PS5_Browser_appCache_remove",
+        binarySource: "",
+        version: "1.0",
+        customAction: CUSTOM_ACTION_APPCACHE_REMOVE
+    }
+
+];

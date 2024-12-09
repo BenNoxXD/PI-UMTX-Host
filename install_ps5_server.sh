db6af4ad68cc20f6e94c58df3e8d5f04b8566f9f
BenNoxXD <flippydiode@gmail.com>
2024-12-09 19:35:34 +0100

    Update install_ps5_server.sh

--- install_ps5_server.sh
+++ install_ps5_server.sh
@@ -8,21 +8,21 @@ rm /etc/systemd/system/ps5host.service
 cd /usr/local
 sudo rm -r PS5-UMTX-Jailbreak
 # download new version
-git clone https://github.com/idlesauce/PS5-UMTX-Jailbreak
+sudo git clone https://github.com/idlesauce/PS5-UMTX-Jailbreak
 cd PS5-UMTX-Jailbreak
-wget https://raw.githubusercontent.com/idlesauce/PS5-Exploit-Host/refs/heads/main/simple_server.py
-rm -r document/en/ps5/payloads
-git clone https://github.com/BenNoxXD/PI-UMTX-Host/
-cp -r PI-UMTX-Host/payload_map.js document/en/ps5
-cp -r PI-UMTX-Host/payloads document/en/ps5/
-rm -r PI-UMTX-Host
+sudo wget https://raw.githubusercontent.com/idlesauce/PS5-Exploit-Host/refs/heads/main/simple_server.py
+sudo rm -r document/en/ps5/payloads
+sudo git clone https://github.com/BenNoxXD/PI-UMTX-Host/
+sudo cp -r PI-UMTX-Host/payload_map.js document/en/ps5
+sudo cp -r PI-UMTX-Host/payloads document/en/ps5/
+sudo rm -r PI-UMTX-Host
 # create a service for autostart
 cat > /etc/systemd/system/ps5host.service <<- "EOF"
 [Unit]
 Description=PS5 UMTX Host idlesauce
 
 [Service]
-WorkingDirectory=/home/PS5-UMTX-Jailbreak/
+WorkingDirectory=/usr/local/PS5-UMTX-Jailbreak/
 ExecStart=/usr/bin/python3 simple_server.py
 
 [Install]
@@ -31,4 +31,4 @@ EOF
 # enable the service
 sudo systemctl start ps5host
 sudo systemctl enable ps5host
-sudo reboot
+# sudo reboot

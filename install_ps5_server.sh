c28c4c956d78d24563753016619c7bbbd68dd2e7
BenNoxXD <flippydiode@gmail.com>
2024-12-09 19:22:26 +0100

    Update install_ps5_server.sh

--- install_ps5_server.sh
+++ install_ps5_server.sh
@@ -5,7 +5,7 @@ sudo apt-get install python3 -y
 sudo systemctl stop ps5host
 sudo systemctl disable ps5host
 rm /etc/systemd/system/ps5host.service
-cd /home
+cd /usr/local
 sudo rm -r /PS5-UMTX-Jailbreak
 # download new version
 git clone https://github.com/idlesauce/PS5-UMTX-Jailbreak
@@ -13,8 +13,9 @@ cd PS5-UMTX-Jailbreak
 wget https://raw.githubusercontent.com/idlesauce/PS5-Exploit-Host/refs/heads/main/simple_server.py
 rm -r document/en/ps5/payloads
 git clone https://github.com/BenNoxXD/PI-UMTX-Host/
-cp -r PI-UMTX-Host/payload_map.js document/en/ps5
-cp -r PI-UMTX-Host/payloads document/en/ps5/
+mv -r PI-UMTX-Host/payload_map.js document/en/ps5
+mv -r PI-UMTX-Host/payloads document/en/ps5/
+rm -r PI-UMTX-Host
 # create a service for autostart
 cat > /etc/systemd/system/ps5host.service <<- "EOF"
 [Unit]
